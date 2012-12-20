00a600252fe2c6e2635457d591e6238ac3cac011
Horacio Alonso <horo.alonso.sanabria@gmail.com>
2012-12-20 00:21:58 -0300

    Initial Commit

---
+++ gen/com/ndigitales/android/superlistado/paises/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ndigitales.android.superlistado.paises;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
